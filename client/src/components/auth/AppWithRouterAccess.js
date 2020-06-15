0e57609cd99b81f77d01c8a21608b861bc6af377
Cale Switzer <Tennyx@Cales-MacBook-Pro.local>
2020-06-15 14:36:39 -0400

    have sign-in widget working for logging-in, registering users. MFA SMS currently activated.

---
+++ client/src/components/auth/AppWithRouterAccess.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Route, withRouter } from 'react-router-dom';
+import { Security, SecureRoute, LoginCallback } from '@okta/okta-react';
+import Home from '../home/Home';
+import Login from './Login';
+import Protected from './Protected';
+
+export default withRouter(class AppWithRouterAccess extends Component {
+	constructor(props) {
+		super(props);
+		this.onAuthRequired = this.onAuthRequired.bind(this);
+	}
+
+	onAuthRequired() {
+		this.props.history.push('/login');
+	}
+
+	render() {
+		return (
+			<Security issuer='https://dev-477362.okta.com/oauth2/default'
+			clientId='0oafalkvxdifq1B684x6'
+			redirectUri={window.location.origin + '/implicit/callback'}
+			onAuthRequired={this.onAuthRequired}
+			pkce={true}>
+				<Route path='/' exact={true} component={Home} />
+				<SecureRoute path='/protected' component={Protected} />
+				<Route path='/login' render={() => <Login baseUrl='https://dev-477362.okta.com' />} />
+				<Route path='/implicit/callback' component={LoginCallback} />
+			</Security>
+		);
+	}
+});
